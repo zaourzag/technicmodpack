d44327068e024d63db7b1f0799bf6457c0012431
Zakaria aourzag <29924334+zaourzag@users.noreply.github.com>
2021-01-18 17:39:45 +0100

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,5 @@
+curl -lo modpack.jar https://ci.codemc.io/job/Mohist-Community/job/Mohist-1.12.2/178/artifact/projects/mohist/build/libs/mohist-1.12.2-178-server.jar 
+wget https://github.com/zaourzag/technicmodpack/raw/main/technicserver-all-2.0.2-rc2.jar
+wget https://raw.githubusercontent.com/zaourzag/technicmodpack/main/modpack.properties
+
+echo "edit modpack.properties with a correct modpack url and then hit start"
