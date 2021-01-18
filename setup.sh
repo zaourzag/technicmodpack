2e1454a54a0fb7d8fd4c3a0df1b60b64e6502e11
Zakaria aourzag <29924334+zaourzag@users.noreply.github.com>
2021-01-18 17:41:26 +0100

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,3 @@
+curl https://raw.githubusercontent.com/zaourzag/technicmodpack/main/install.sh | bash
+wget https://raw.githubusercontent.com/zaourzag/technicmodpack/main/init.sh
+wget https://raw.githubusercontent.com/zaourzag/technicmodpack/main/start.sh
