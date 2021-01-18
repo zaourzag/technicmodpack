819a57a2c6f6a56f30c14a5b7b0c10698703ffad
Zakaria aourzag <29924334+zaourzag@users.noreply.github.com>
2021-01-18 17:40:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-curl -lo modpack.jar https://ci.codemc.io/job/Mohist-Community/job/Mohist-1.12.2/178/artifact/projects/mohist/build/libs/mohist-1.12.2-178-server.jar 
+
 wget https://github.com/zaourzag/technicmodpack/raw/main/technicserver-all-2.0.2-rc2.jar
 wget https://raw.githubusercontent.com/zaourzag/technicmodpack/main/modpack.properties
 
